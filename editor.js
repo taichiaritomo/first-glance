8565097ae6b85b3fc1bfd4176dd8b8696719d4b6
Taichi Aritomo <taichiaritomo@gmail.com>
2017-05-28 19:46:30 -0400

    Group A, Ready

--- editor.js
+++ editor.js
@@ -43,96 +43,11 @@ var editor = new Quill("#editor", {
 //	}
 //]);
 
-//editor.setContents([
-//    {
-//		insert: "Notice: ",
-//		attributes: { size: "large" }
-//	},
-//    {
-//		insert: "Changes to Public Service Billing",
-//		attributes: { size: "large", bold: true }
-//	},
-//    {
-//        insert: "\nfor residents of Salt Lake City",
-//        attributes: { bold: true }
-//    },
-//    {
-//        insert: "\n\nCity meters are read and billed monthly."
-//    },
-//    {
-//        insert: "\nBills include charges for water service, garbage pickup, sewer service, a storm water charge, and a franchise fee of 6% of the water and sewer charges.",
-//        attributes: { color: "#999999" }
-//    },
-//    {
-//        insert: "\n\nCustomers living outside city boundaries are billed for water service only."
-//    },
-//    {
-//        insert: "\n\nBills are due 15 days after the mailing date. ",
-//        attributes: { bold: true }
-//    },
-//    {
-//        insert: "If service is terminated because of a past due bill or rules violations, before service is restored, a reconnection fee will be assessed and collected in addition to the delinquent amount."
-//    }
-//]);
 
-var experiment_use_hotplate = true;
+
 
 var glance_view = false,
 	heat_view = false;
-
-if (experiment_use_hotplate) {
-    editor.setContents([
-        {
-            insert: "The Tool",
-            attributes: { size: "large", color: "#666666", bold: true }
-        },
-        {
-            insert: "\n\nYou will be using this simple text-editor.",
-            attributes: { color: "#666666" }
-        },
-        {
-            insert: "\n\nBehind your text, you'll see orange spots. The orange represents ",
-            attributes: { color: "#666666" }
-        },
-        {
-            insert: "visual saliency",
-            attributes: { color: "#666666", bold: true }
-        },
-        {
-            insert: ", the tendency for visual attention to be attracted to that location. As you make changes, the text-editor predicts what areas attract the most visual attention.",
-            attributes: { color: "#666666", bold: false }
-        },
-        {
-            insert: "\n\nYou can use the pin button to save a version of your work for comparison.",
-            attributes: { color: "#666666" }
-        }
-    ]);
-} else {
-    editor.setContents([
-        {
-            insert: "The Tool",
-            attributes: { size: "large", color: "#666666", bold: true }
-        },
-        {
-            insert: "\n\nYou will be using this simple text-editor.",
-            attributes: { color: "#666666" }
-        },
-        {
-            insert: "\n\nYou can use the pin button to save a version of your work for comparison.",
-            attributes: { color: "#666666" }
-        }
-    ]);
-}
-
-
-window.onload = function() {
-//	document.querySelector(".ql-editor").focus();
-    if (experiment_use_hotplate) {
-        document.querySelector("#hotplate-button").click();
-    } else {
-        document.querySelector("#hotplate-button").style.display = "none";
-    }
-};
 
 
 function heatButtonHandler() {
---
+++ experiment_a.js
@@ -0,0 +1,117 @@
+editor.setContents([
+    {
+        insert: "Whitesville DMV\nDepartment of Motor Vehicles\n\nLicenses / Permits\nSee Desks 1-3 to renew or replace your driver's license. Please have your 6-point ID ready when you reach the desk. You can also renew your license by a mail-in form that you can get at the front desk.\nSee Desk 4 to schedule a road or permit test. If you are taking a permit test, check in at the Testing Center downstairs. Road tests are conducted at the Springfield DMV.\n\nVehicle\nSee Desks 5-6 for vehicle ownership and registration issues. You can also renew your registration, pay tickets, and order license plates online at www.state.nj.us/mvc\n\nIf your issue is not listed here, ask the front desk for information."
+    }
+]);
+
+var experiment_use_hotplate = true;
+
+window.onload = function() {
+//	document.querySelector(".ql-editor").focus();
+    if (experiment_use_hotplate) {
+        document.querySelector("#hotplate-button").click();
+    } else {
+        document.querySelector("#hotplate-button").style.display = "none";
+    }
+//    editor.disable();
+};
+
+
+
+
+
+editor.disable();
+
+document.querySelector("#intro-continue").addEventListener("click", function() {
+    document.querySelector("#intro-text").classList.add("experiment-hide");
+    setTimeout(function() {
+        document.querySelector("#intro-text").style.display = "none";
+        document.querySelector("#task-text").style.display = "block";
+        setTimeout(function() {
+            document.querySelector("#task-text").classList.add("experiment-show");
+        }, 100);
+    }, 500);
+});
+
+document.querySelector("#next-1").addEventListener("click", function() {
+    document.querySelector("#next-1").style.visibility = "hidden";
+    document.querySelector("p#next-1-target").classList.remove("experiment-hide");
+    document.querySelector("#container").classList.remove("experiment-hide");
+});
+
+var taskStarted = false;
+
+document.querySelector("#task-start-button").addEventListener("click", function() {
+    if (!taskStarted) {
+        startTimer();
+    } else {
+        /* finish task */
+    }
+});
+
+var taskFinishTime;
+
+function startTimer() {
+    /* Enable editor */
+    editor.enable();
+    editor.focus();
+    document.querySelector("#task-start-button").style.display = "none";
+    document.querySelector("#container").classList.remove("experiment-disable");
+    
+    /* Make task-info fade */
+    document.querySelector("#experiment-info").classList.add("fade");
+    
+    /* */
+    document.querySelector("#task-time-container").classList.remove("experiment-hide");
+    document.querySelector("#task-done-button").classList.remove("experiment-hide");
+    document.querySelector("#pin-button").click();
+    
+    var currentTime = new Date();
+    taskFinishTime = new Date(currentTime.getTime() + 600000);
+    var x = setInterval(function() {
+        var now = new Date().getTime(),
+            distance = taskFinishTime - now,
+            minutes = Math.floor((distance % (1000*60*60)) / (1000*60)),
+            seconds = Math.floor((distance % (1000*60)) / 1000);
+        
+        if (minutes < 10) {minutes = "0" + minutes;}
+        if (seconds < 10) {seconds = "0" + seconds;}
+        
+        document.querySelector("#task-time-remaining").innerHTML = minutes + ":" + seconds;
+        
+        if (distance < 0) {
+            clearInterval(x);
+            finishTask();
+        }
+    }, 1000);
+}
+
+document.querySelector("#task-done-button").addEventListener("click", function() {
+    finishTask();
+});
+
+var finishTask = function() {
+    editor.disable();
+    document.querySelector("#container").classList.add("experiment-disable");
+    document.querySelector("#experiment-info").classList.remove("fade");
+    
+    var ql = document.querySelector("#editor");
+    domtoimage.toPng(ql, { bgcolor: "white" })
+    .then(function (dataUrl) {
+        var link = document.querySelector("#text-image-download");
+        link.download = 'your-info-card-design';
+        link.href = dataUrl;
+    });
+    
+    document.querySelector("#task-text").classList.add("experiment-hide");
+    setTimeout(function() {
+        document.querySelector("#task-text").style.display = "none";
+        document.querySelector("#final-text").style.display = "block";
+        setTimeout(function() {
+            document.querySelector("#final-text").classList.add("experiment-show");
+        }, 100);
+    }, 500);
+}
+
+
+
