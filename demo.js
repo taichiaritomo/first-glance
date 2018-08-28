05bad1b185d813a5475005a789dc9ac47d3624f5
Taichi Aritomo <taichiaritomo@gmail.com>
2018-08-27 21:57:52 -0400

    font change, description change

--- demo.js
+++ demo.js
@@ -1,38 +1,77 @@
 editor.setContents([
     {
-        insert: "Look.as\n",
-        attributes: { bold: true, italic: true, size: "large" }
+        insert: "\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\n\\/ ",
+        attributes: { align: "center" }
     },
     {
-        insert: "is an experimental design tool that tells you how text information ",
-        attributes: { size: "large" }
+        insert: "LOOK.AS",
+        attributes: { align: "center", bold: true }
     },
     {
-        insert: "stands out",
-        attributes: { italic: true, size: "large" }
+        insert: " /\\/ is \\/\\ a \\/\\ ",
+        attributes: { align: "center" }
     },
     {
-        insert: " visually as parts of a whole.\n\n",
-        attributes: { size: "large" }
+        insert: "text-editor",
+        attributes: { align: "center", underline: true }
     },
     {
-        insert: "\nOn the right is a "
+        insert: " \\/\n\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\n\\/ that \\/\\ looks \\/\\ at /\\/ your \\/\\/\\/\\/\n\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\n\\/ work \\/\\ with /\\/ ",
+        attributes: { align: "center"}
     },
     {
-        insert: "saliency map",
-        attributes: { italic: true }
+        insert: "fresh eyes",
+        attributes: { align: "center", bold: true, italic: true }
     },
     {
-        insert: " of this text.\nThe intensity of white corresponds to the likelihood that a human viewer will gaze at that point first, according to a computer vision algorithm that models visual saliency*."
+        insert: " \\/\\/\\/\\/\\/\n\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\n\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\n\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\n\\/ It \\/\\ uses /\\/ spectral analysis /\\/\\/\n\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\n\\/ to \\/\\ highlight \\/\\ regions \\/\\/\\/\\/\\/\n\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\n\\/ that \\/\\ are \\/\\ likely /\\/ to \\/\\/\\/\\/\n\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\n\\/ attract /\\/ ",
+        attributes: { align: "center"}
     },
     {
-        insert: "\n\n\n"
+        insert: "visual attention",
+        attributes: { align: "center", bold: true, italic: true }
     },
     {
-        insert: '*Xiaodi Hou and Liqing Zhang. "Saliency detection: A spectral residual approach."',
-        attributes: { size: "small", color: "#999" }
+        insert: " \\/\\/\\/\\/\\/\n\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\n\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\n",
+        attributes: { align: "center"}
     }
 ]);
+
+//editor.setContents([
+//    {
+//        insert: "Look.as\n",
+//        attributes: { bold: true, italic: true, size: "large" }
+//    },
+//    {
+//        insert: "is an experimental design tool that tells you how text information ",
+//        attributes: { size: "large" }
+//    },
+//    {
+//        insert: "stands out",
+//        attributes: { italic: true, size: "large" }
+//    },
+//    {
+//        insert: " visually as parts of a whole.\n\n",
+//        attributes: { size: "large" }
+//    },
+//    {
+//        insert: "\nOn the right is a "
+//    },
+//    {
+//        insert: "saliency map",
+//        attributes: { italic: true }
+//    },
+//    {
+//        insert: " of this text.\nThe intensity of white corresponds to the likelihood that a human viewer will gaze at that point first, according to a computer vision algorithm that models visual saliency*."
+//    },
+//    {
+//        insert: "\n\n\n"
+//    },
+//    {
+//        insert: '*Xiaodi Hou and Liqing Zhang. "Saliency detection: A spectral residual approach."',
+//        attributes: { size: "small", color: "#999" }
+//    }
+//]);
 
 //editor.setContents([
 //    {
